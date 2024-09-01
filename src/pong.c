758d3dfaabec9f03840f08490b3e077170880dd8
Ro0kly <snallytango@yandex.ru>
2024-09-01 20:01:30 +0300

    main / first commit + add variables

---
+++ src/pong.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int shirina = 80, visota = 25;             //параметры поля
+    int xshar = 10, yshar = 10;                // позиция мяча
+    int xrocket1 = 1, yrocket1 = 9;            // позиция первой ракетки
+    int xrocket2 = shirina - 2, yrocket2 = 9;  // позиция второй ракетки
+    int score1 = 0, score2 = 0;
+
+    int v = 0, h = 0;  // флажки направлений
+    char c;
+    char a;
+    char turn = 'l';
+}
