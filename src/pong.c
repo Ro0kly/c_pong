6eda9f47393ee4d3da23f40a795a046776b46649
Ro0kly <snallytango@yandex.ru>
2024-09-02 11:35:39 +0300

    main / add methods

--- src/pong.c
+++ src/pong.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+void screen(int x, int y, int shir, int vis, int xrocket1, int yrocket1, int xrocket2, int yrocket2, int left,
+            int right);                  //вывод
+int smenax(int x, int vertic, int vis);  //смена направления мяча вниз-вверх
+int smenay(int x, int y, int horiz, int shir, int xrocket1,
+           int xrocket2);          //смена направления мяча вправо-влево
+int movesharx(int x, int vertic);  // движение мяча
+int moveshary(int y, int horiz);
+int defaultpos(int y, int shirina);
+
+int proverka(int x, int left, int right, int shirina);
+void itogimatcha(int score1, int score2);
+
+int get_new_coordinate(int y, char direction);
+
 int main() {
     int shirina = 80, visota = 25;             //параметры поля
     int xshar = 10, yshar = 10;                // позиция мяча
