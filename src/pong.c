8e1a9c5b1eba660881e020b7a30b88b6ef269e20
Ro0kly <snallytango@yandex.ru>
2024-09-07 15:53:50 +0300

    main / complete pong

--- src/pong.c
+++ src/pong.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 void screen(int x, int y, int shir, int vis, int xrocket1, int yrocket1, int xrocket2, int yrocket2, int left,
             int right);                  //вывод
 int smenax(int x, int vertic, int vis);  //смена направления мяча вниз-вверх
@@ -24,7 +23,82 @@ int main() {
     int v = 0, h = 0;  // флажки направлений
     char c;
     char a;
+    screen(yshar, xshar, shirina, visota, xrocket1, yrocket1, xrocket2, yrocket2, score1, score2);
+
     char turn = 'l';
+
+    while (score1 != 21 && score2 != 21) {
+        if (yshar == 1)
+            score1++;
+        else if (yshar == shirina - 2)
+            score2++;
+
+        scanf("%c%c", &a, &c);
+        printf("input is:%c%c, turn is:%c\n", a, c, turn);
+
+        if ((yrocket1 == 2 && a == 'a') || (yrocket2 == 2 && a == 'k')) {
+            printf("you can't move\n");
+        } else if ((yrocket1 == 22 && a == 'z') || (yrocket2 == 22 && a == 'm')) {
+            printf("you can't move\n");
+        } else {
+            if (a == ' ') {
+                if (turn == 'l') {
+                    printf("skip move r\n");
+                    turn = 'r';
+                } else {
+                    printf("skip move l\n");
+                    turn = 'l';
+                }
+                v = smenax(xshar, v, visota);
+                h = smenay(xshar, yshar, h, shirina, yrocket1, yrocket2);
+                xshar = movesharx(xshar, v);
+                yshar = moveshary(yshar, h);
+                yshar = defaultpos(yshar, shirina);
+            } else {
+                if (turn == 'l') {
+                    if (a == 'k' || a == 'm') {
+                        printf("not your turn\n");
+                    } else {
+                        if (a == 'a') {
+                            printf("go top\n");
+                            yrocket1 = get_new_coordinate(yrocket1, 't');
+                        }
+                        if (a == 'z') {
+                            printf("go bottom\n");
+                            yrocket1 = get_new_coordinate(yrocket1, 'b');
+                        }
+                        v = smenax(xshar, v, visota);
+                        h = smenay(xshar, yshar, h, shirina, yrocket1, yrocket2);
+                        xshar = movesharx(xshar, v);
+                        yshar = moveshary(yshar, h);
+                        yshar = defaultpos(yshar, shirina);
+                        turn = 'r';
+                    }
+                } else if (turn == 'r') {
+                    if (a == 'a' || a == 'z') {
+                        printf("not your turn\n");
+                    } else {
+                        if (a == 'k') {
+                            printf("go top\n");
+                            yrocket2 = get_new_coordinate(yrocket2, 't');
+                        }
+                        if (a == 'm') {
+                            printf("go bottom\n");
+                            yrocket2 = get_new_coordinate(yrocket2, 'b');
+                        }
+                        v = smenax(xshar, v, visota);
+                        h = smenay(xshar, yshar, h, shirina, yrocket1, yrocket2);
+                        xshar = movesharx(xshar, v);
+                        yshar = moveshary(yshar, h);
+                        yshar = defaultpos(yshar, shirina);
+                        turn = 'l';
+                    }
+                }
+            }
+        }
+        screen(yshar, xshar, shirina, visota, xrocket1, yrocket1, xrocket2, yrocket2, score1, score2);
+    }
+    itogimatcha(score1, score2);
 }
 
 int get_new_coordinate(int y, char direction) {
@@ -35,6 +109,58 @@ int get_new_coordinate(int y, char direction) {
         _y += 1;
     }
     return _y;
+}
+
+void screen(int x, int y, int shir, int vis, int xrocket1, int yrocket1, int xrocket2, int yrocket2, int left,
+            int right) {
+    for (int i = 0; i <= vis; i++) {
+        for (int j = 0; j < shir; j++) {
+            if (i == 0 && j == 36) {
+                printf("%d", left);
+                j++;
+            }
+            if (i == 0 && j == 44) {
+                printf("%d", right);
+                j++;
+            }
+
+            if (i == 1 || i == vis || ((j == 0 || j == shir - 1) && i != 0))
+                printf("#");
+            else {
+                if ((i >= yrocket1 && i <= yrocket1 + 2 && j == xrocket1) ||
+                    (i >= yrocket2 && i <= yrocket2 + 2 && j == xrocket2))
+                    printf("|");
+                else if (i == y && j == x)
+                    printf("*");
+                else if (j == 40)
+                    printf("-");
+                else
+                    printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int smenax(int x, int vertic, int vis) {
+    // vertic 0 - vverh 1 - vniz
+    if (x == 1 && vertic == 0)
+        vertic = 1;
+    else if (vis - x == 1 && vertic == 1)
+        vertic = 0;
+    return vertic;
+}
+
+int smenay(int x, int y, int horiz, int shir, int xrocket1, int xrocket2) {
+    // horiz 0 - vpravo 1 - vlevo
+    if (y == 2 && horiz == 1) {
+        if (x >= xrocket1 && x <= xrocket1 + 2) horiz = 0;
+    }
+    //  else
+    else if (shir - y == 3 && horiz == 0) {
+        if (x >= xrocket2 && x <= xrocket2 + 2) horiz = 1;
+    }
+    return horiz;
 }
 
 int movesharx(int x, int vertic) {
@@ -51,6 +177,13 @@ int moveshary(int y, int horiz) {
     else
         y = y - 1;
     return y;
+}
+
+int defaultpos(int y, int shirina) {
+    if (y <= 0 || y >= shirina)
+        return shirina / 2;
+    else
+        return y;
 }
 
 void itogimatcha(int score1, int score2) {
